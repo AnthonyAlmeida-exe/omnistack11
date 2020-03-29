50c247a3489236d0eaebc1b1904d6f14373464d7
Anthony Almeida <anthony.almeida@trinitysolucoes.com>
2020-03-28 21:02:40 -0300

    :construction: Back end complet

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
+const OngController = require("./controllers/OngController");
+const IncidentsController = require("./controllers/IncidentsController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
 const routes = express.Router();
+routes.post("/sessions", SessionController.create);
 
-routes.post("/users", (req, res) => {
-  return res.json({ ok: false });
-});
+routes.post("/ongs", OngController.create);
+routes.get("/ongs", OngController.index);
 
+//incidents
+routes.post("/incidents", IncidentsController.create);
+routes.get("/incidents", IncidentsController.index);
+routes.delete("/incidents/:id", IncidentsController.delete);
+
+//ProfileIncidents
+routes.get("/profile", ProfileController.index);
 module.exports = routes;
